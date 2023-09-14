1aebd0aa28a122c4b8084c092b6c5edfe11a9093
John Winsor <johnwinsor@gmail.com>
2023-09-13 23:54:09 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,61 @@
 import Image from 'next/image'
 
-export default function Home() {
+async function getData() {
+  const items = []
+  const res = await fetch(
+    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/acq/po-lines?q=item_library~MAIN&status=ALL&limit=5&offset=0&order_by=title&direction=desc&acquisition_method=ALL&expand=notes&min_expected_arrival_date=2022-03-06&max_expected_arrival_date=2023-04-06&apikey=l7xx2af7939c63424511946e0fcdc35fe22a',
+    {
+      method: 'GET',
+      headers: {
+          'accept': 'application/json',
+      },
+    }
+    
+  )
+  const data = await res.json()
+  for (const line of data.po_line) {
+    console.log(line.link+"?apikey=l7xx2af7939c63424511946e0fcdc35fe22a")
+    const response = await fetch(
+      `${line.link}?apikey=l7xx2af7939c63424511946e0fcdc35fe22a`,
+      {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+        },
+      }
+      )
+    const item = await response.json();
+    console.log(item.resource_metadata.title)
+    items.push(item)
+  }
+  return items
+}
+
+export default async function Home() {
+  const data = await getData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="m-2 grid grid-cols-12 gap-2">
+        {data.map(d => (
+          <div 
+            key={d.number}
+            className="col-span-6 md:col-span-4 lg:col-span-3 aspect-video w-full bg-gray-100 relative"
+            >
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-stone-950`}>
+              {d.resource_metadata.title}
+              </p>
+            <Image 
+              src={`https://covers.openlibrary.org/b/isbn/${d.resource_metadata.isbn}-M.jpg`}
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+              />
+          </div>
+        ))}
+      </div>
+      
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
