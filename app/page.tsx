d2d8e318d6a7e44e49f9687dba5743cf127e9c93
John Winsor <johnwinsor@gmail.com>
2023-10-11 23:53:23 -0700

    load from data.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import Image from 'next/image'
 //import getData from "@/components/getJson"
-import getXml from "@/components/getXml"
+//import getXml from "@/components/getXml"
 import Carousel from "@/components/EmblaCarousel";
 import './css/embla.css'
+import { promises as fs } from 'fs';
 
 export default async function Home() {
   //const data = await getData()
   //console.log(data)
-  const xml = await getXml()
+  //const xml = await getXml()
   //console.log(xml)
+  const jsonString = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
+  const data = JSON.parse(jsonString);
+  console.log(data)
   return (
     <main className="flex-none h-screen flex-col items-center justify-between">
       <div className="h-full mx-auto">
         <h1 className="text-center font-bold pt-6 h-12 text-3xl">New Arrivals</h1>
         <Carousel loop>
-          {xml.map((src, i) => {
+          {data.map((src:any, i:any) => {
+            const callnoStatus = src.callNo.replace(/Unknown/g, "In Processing");
             return (
               <div className="w-auto h-full flex-[0_0_100%] mx-auto" key={i}>
                 <div className="relative h-5/6">
                   <Image
-                    src={src.olCoverURL}
+                    src={src.coverurl}
                     fill={true}
                     className="mx-auto object-contain"
                     alt="alt"
@@ -29,7 +34,7 @@ export default async function Home() {
                 <div className="relative caption mx-auto h-1/6">
                   <h1 className="text-center font-bold">{src.title}</h1>
                   <h1 className="text-center">{src.author}</h1>
-                  <h1 className="text-center">Call Number: {src.callno}</h1>
+                  <h1 className="text-center">Call Number: {callnoStatus}</h1>
                   <h1 className="text-center">Received: {src.recDate}</h1>
                 </div>
               </div>
