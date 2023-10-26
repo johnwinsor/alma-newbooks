bb81c441f0fcd5afa5ace977b668eccc46c63f9c
John Winsor <johnwinsor@gmail.com>
2023-10-26 13:32:27 -0700

    base path comment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // basePath: '/monitor',
     images: {
         domains: ['covers.openlibrary.org', 'www.googleapis.com','books.google.com','pictures.abebooks.com','m.media-amazon.com'],
     },
