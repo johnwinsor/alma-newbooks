1062f83e1e492e31aaa29a79f31883b120da14c5
John Winsor <johnwinsor@gmail.com>
2023-11-01 22:50:52 -0700

    import

--- app/(widgets)/data.tsx
+++ app/(widgets)/data.tsx
@@ -1,8 +1,40 @@
-import { promises as fs } from 'fs';
+'use client'
 
-export async function getData() {
-  const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
-  const data = JSON.parse(file);
+import jsonFile from '@/app/data.json';
+import Carousel from "@/components/EmblaCarousel";
+import Image from 'next/image'
 
-  return data
-}
+export async function GetData() {
+    const data = jsonFile;
+
+    return (
+        <main className="flex-none h-screen flex-col items-center justify-between bg-slate-900">
+        <div className="h-full mx-auto">
+        <div className="text-center font-bold pt-4 h-12 text-2xl">New Arrivals</div>
+        <Carousel loop>
+          {data.map((src:any, i:any) => {
+            const callnoStatus = src.callNo.replace(/Unknown/g, "In Processing");
+            return (
+              <div className="w-auto h-full flex-[0_0_100%] mx-auto" key={i}>
+                <div className="relative h-5/6">
+                  <Image
+                    src={src.coverurl}
+                    fill={true}
+                    className="mx-auto object-contain"
+                    alt="alt"
+                  />
+                </div>
+                <div className="relative caption mx-auto h-1/6">
+                  <h1 className="text-center font-bold">{src.title}</h1>
+                  <h1 className="text-center">{src.author}</h1>
+                  <h1 className="text-center">Call Number: {callnoStatus}</h1>
+                  <h1 className="text-center">Received: {src.recDate}</h1>
+                </div>
+              </div>
+            );
+          })}
+        </Carousel>
+        </div>
+        </main>
+    )
+  }
--- app/(widgets)/monitor/page.tsx
+++ app/(widgets)/monitor/page.tsx
@@ -1,39 +1,9 @@
 import React from "react";
-import Image from 'next/image'
-import Carousel from "@/components/EmblaCarousel";
 import '../css/embla.css'
-import { getData } from "@/app/(widgets)/data";
+import { GetData } from "@/app/(widgets)/data"
 
 export default async function Home() {
-  const data = await getData();
   return (
-    <main className="flex-none h-screen flex-col items-center justify-between bg-slate-900">
-      <div className="h-full mx-auto">
-        <div className="text-center font-bold pt-4 h-12 text-2xl">New Arrivals</div>
-        <Carousel loop>
-          {data.map((src:any, i:any) => {
-            const callnoStatus = src.callNo.replace(/Unknown/g, "In Processing");
-            return (
-              <div className="w-auto h-full flex-[0_0_100%] mx-auto" key={i}>
-                <div className="relative h-5/6">
-                  <Image
-                    src={src.coverurl}
-                    fill={true}
-                    className="mx-auto object-contain"
-                    alt="alt"
-                  />
-                </div>
-                <div className="relative caption mx-auto h-1/6">
-                  <h1 className="text-center font-bold">{src.title}</h1>
-                  <h1 className="text-center">{src.author}</h1>
-                  <h1 className="text-center">Call Number: {callnoStatus}</h1>
-                  <h1 className="text-center">Received: {src.recDate}</h1>
-                </div>
-              </div>
-            );
-          })}
-        </Carousel>
-      </div>
-    </main>
+      <GetData />
   )
 }
