d11a3bd3461621a9e7046190834361a05cdbaadb
John Winsor <johnwinsor@gmail.com>
2023-11-03 09:11:39 -0700

    base path=widgets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/monitor',
+    basePath: '/widgets',
     images: {
         domains: ['covers.openlibrary.org', 'www.googleapis.com','books.google.com','pictures.abebooks.com','m.media-amazon.com'],
     },
